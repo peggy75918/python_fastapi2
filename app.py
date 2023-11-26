da309b115c0ce35c1ff906ccfb9bf156e58d1e17
Peggy <peggy4328@gmail.com>
2023-11-27 04:54:57 +0800

    add path

--- app.py
+++ app.py
@@ -4,9 +4,30 @@ from homework import homework
 
 app = FastAPI()
 
-@app.get("/homework")
-def root():
+@app.get('/homework')
+def get_all_homeworks() -> dict:
     return homework
+
+
+@app.get('/school')
+def get_homeworks_by_school(school: str = "") -> dict:
+    return homework[school]
+    
+
+
+@app.get('/semester')
+def get_homeworks_by_semester(semester: str = "") -> list:
+    ntue = homework["ntue"][semester]
+    ntut = homework["ntut"][semester]
+
+    return [*ntue, *ntut]
+
+
+@app.get('/school/semester')
+def get_homeworks_by_semester(school: str = "", semester: str = "") -> list:
+    return homework[school][semester]
+
+
 
 if __name__ == "__main__":
     uvicorn.run("app:app", port=5000, reload=True)
